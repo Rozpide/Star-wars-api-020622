39772e8ad5aab74f0684613486c4b84e1e7efc7b
Rozpi <joserozpide@yahoo.es>
2022-06-02 21:05:13 +0000

    SW-020522

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+// import logo from "/workspace/react-hello-webapp/src/js/component/start-wars.jpg"
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1"><img src="https://static3.srcdn.com/wordpress/wp-content/uploads/Star-Wars-Episode-7-Fan-Logo.jpg" width={"300px"}></img></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
